7f548a503af9661d1217b6767fcb3478da7816a2
e60937u <pierre-yves.le-gall5@etu.univ-lorraine.fr>
2025-05-23 11:35:01 +0200

    premiers tests simples

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,1 +1,38 @@
-// Write your test here
+import { expect, test } from 'vitest';
+
+class RomanNumeral {
+  of(value: string): number {
+    let result = 0;
+    // let romanNumber: string[] = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+
+    if (value === 'I') return 1;
+    if (value === 'IV') return 4;
+    if (value === 'V') return 5;
+    if (value === 'X') return 10;
+    if (value === 'L') return 50;
+    if (value === 'C') return 100;
+    if (value === 'D') return 500;
+    if (value === 'M') return 1000;
+
+    return result;
+  }
+}
+
+test.each([
+  ['I', 1],
+  ['V', 5],
+  ['X', 10],
+  ['L', 50],
+  ['C', 100],
+  ['D', 500],
+  ['M', 1000],
+])('%s should give %s', (input, expected) => {
+  console.log(input, expected);
+  expect(new RomanNumeral().of('I')).toBe(1);
+  expect(new RomanNumeral().of('V')).toBe(5);
+  expect(new RomanNumeral().of('X')).toBe(10);
+  expect(new RomanNumeral().of('L')).toBe(50);
+  expect(new RomanNumeral().of('C')).toBe(100);
+  expect(new RomanNumeral().of('D')).toBe(500);
+  expect(new RomanNumeral().of('M')).toBe(1000);
+});
